5b95135387a2796706b65b4fa5645e2ae7fa76a7
Mazen Thabet <MazenThabet2@outlook.com>
2026-03-28 15:32:45 +0200

    Add project files.

---
+++ G-NET-26-Advacned-01/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace G_NET_26_Advacned_01
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            #region Q1
+            //Q1: What is a generic class? Why use generics?
+            //Answer:
+            //A Generic class is a class definition that acts as a template
+            //that can be used to write reusable code that can work with any data type.
+            //Why we use Generics:
+            //To make the code more reusable and optimize performance, as 
+            //we can use the same template for different data types, on 
+            //the other hand, Object class can also use the same template 
+            //for different data types using boxing/unboxing
+            //But it will not be optimized as the boxing/unboxing drains 
+            //memory and performance.
+            #endregion
+            #region Q2
+            //Q2: Write a generic class Container<T> with Add and Get methods.
+            #endregion
+            #region Q3
+            //Q3: What are multiple type parameters? Write Pair<TKey, TValue>.
+            #endregion
+            #region Q4
+            //Q4: What is a generic method? Write Swap<T> method.
+            #endregion
+            #region Q5
+            //Q5: Write a generic method FindMax<T> that finds maximum value
+            #endregion
+
+        }
+    }
+}
